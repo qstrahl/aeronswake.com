1376ec1e9eec50c1e56408d867d92d714efdb350
Quinn Strahl <qstrahl@gmail.com>
2014-06-21 06:06:19 +0000

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aeronswakecomApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the aeronswakecomApp
+ */
+angular.module('aeronswakecomApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
