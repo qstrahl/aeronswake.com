097129a776113ad26aba1f5ca2100e35a99221ac
Quinn Strahl <qstrahl@gmail.com>
2014-07-09 22:26:13 -0400

    Add contact page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,10 @@ angular
         templateUrl: 'views/music.html',
         controller: 'MusicCtrl'
       })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aeronswakecomApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the aeronswakecomApp
+ */
+angular.module('aeronswakecomApp')
+  .controller('ContactCtrl', function ($scope) {
+  });
