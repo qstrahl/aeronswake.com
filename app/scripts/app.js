fe6b3537a1e10c1faf2f03dacb3b3de82a3973f6
Quinn Strahl <qstrahl@gmail.com>
2014-07-10 00:20:12 -0400

    Revert "Add contact page"
    This reverts commit 097129a776113ad26aba1f5ca2100e35a99221ac.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,10 +27,6 @@ angular
         templateUrl: 'views/music.html',
         controller: 'MusicCtrl'
       })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl'
-      })
       .otherwise({
         redirectTo: '/'
       });
